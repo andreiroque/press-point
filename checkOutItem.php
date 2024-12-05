433b5f2a32ec28808332f3c3caee7f5c9189f53e
Andrei Roque <roqueandrei0316@gmail.com>
2024-12-05 21:08:26 +0800

    Fix queries for checkOutItem.php to avoid item duplicates in database

--- checkOutItem.php
+++ checkOutItem.php
@@ -10,13 +10,14 @@ if(isset($_SESSION['id']) && isset($_GET['total'])){
   
   $query = "INSERT INTO orders(user_id, total_price) VALUES ('$user_id', '$total_amount')";
   if(mysqli_query($conn, $query)){
-    $query1 = "UPDATE cart SET status='Checked Out' WHERE user_id='$user_id'";
+    $order_id = mysqli_insert_id($conn);
+
+    $query1 = "UPDATE cart SET status='Checked Out' WHERE user_id='$user_id' AND state='New'";
     if(mysqli_query($conn, $query1)){
-      $query2 = "SELECT o.order_id, p.product_id, s.switch_id, c.quantity, p.price FROM cart c INNER JOIN products p ON c.product_id = p.product_id INNER JOIN orders o ON c.user_id = o.user_id INNER JOIN switches s ON c.switch_id = s.switch_id WHERE c.user_id='$user_id'";
+      $query2 = "SELECT p.product_id, s.switch_id, c.quantity, p.price FROM cart c INNER JOIN products p ON c.product_id = p.product_id INNER JOIN switches s ON c.switch_id = s.switch_id WHERE c.user_id='$user_id' AND c.state='New'";
       $result = mysqli_query($conn, $query2);
       if(mysqli_num_rows($result) > 0){
         while($row = mysqli_fetch_assoc($result)){
-          $order_id = $row['order_id'];
           $product_id = $row['product_id'];
           $switch_id = $row['switch_id'];
           $quantity = $row['quantity'];
@@ -26,8 +27,13 @@ if(isset($_SESSION['id']) && isset($_GET['total'])){
           if(mysqli_query($conn, $query3)){
             $query4 = "UPDATE product_variants SET stock = stock - $quantity WHERE product_id ='$product_id' AND switch_id='$switch_id' AND stock >= $quantity";
 
-            if(!mysqli_query($conn, $query4)){
-              echo json_encode(["status" => "error", "message" => "Failed to update stock: " . mysqli_error($conn)]);
+            if(mysqli_query($conn, $query4)){
+                $query5 = "UPDATE cart SET state='Old' WHERE user_id='$user_id' AND state='New' AND status='Checked Out'";
+                if(!mysqli_query($conn, $query5)){
+                    echo json_encode(["status" => "error", "message" => "Failed to update state of cart: " . mysqli_error($conn)]);
+                }
+            }else{
+                echo json_encode(["status" => "error", "message" => "Failed to update stock: " . mysqli_error($conn)]);
             }
 
           }else {
