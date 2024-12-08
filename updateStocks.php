e863466378d5e3becc282e7972275f5795491af4
Andrei Roque <roqueandrei0316@gmail.com>
2024-12-09 00:41:24 +0800

    Modify query to also update the date when the stock is received

--- updateStocks.php
+++ updateStocks.php
@@ -11,7 +11,12 @@ if(isset($_GET['product_id']) && isset($_GET['switch_id']) && isset($_GET['quant
   $query = "UPDATE product_variants SET stock = stock + $quantity WHERE product_id='$product_id' AND switch_id='$switch_id'";
 
   if(mysqli_query($conn, $query)){
-    echo json_encode(["status" => "success", "message" => "Successfully updated stocks!"]);
+    $query1 = "UPDATE products SET received_at = CURRENT_TIMESTAMP() WHERE product_id='$product_id'";
+    if(mysqli_query($conn, $query1)){
+      echo json_encode(["status" => "success", "message" => "Successfully updated stocks!"]);
+    }else{
+      echo json_encode(["status" => "error", "message" => "Failed to update received_at"]);
+    }
   }else{
     echo json_encode(["status" => "error", "message" => "Failed to update stocks!"]);
   }
