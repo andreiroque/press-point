0e49557cf3b5c633c8b70b10e88fa5a32f2aff06
Andrei Roque <roqueandrei0316@gmail.com>
2024-12-09 12:13:18 +0800

    Fix get revenue query to not return null

--- getTotalRevenue.php
+++ getTotalRevenue.php
@@ -3,7 +3,7 @@
 include 'connection.php';
 
 
-$query = "SELECT FORMAT(ROUND(COALESCE(SUM(p.price * oi.quantity) + 150, 0), 2), 2) AS total_revenue FROM  products p INNER JOIN order_items oi ON p.product_id = oi.product_id INNER JOIN orders o ON oi.order_id = o.order_id WHERE o.status = 'Delivered'";
+$query = "SELECT FORMAT(COALESCE(SUM(total_price), 0), 2) AS total_revenue FROM orders WHERE status='Delivered';";
 
 $result = mysqli_query($conn, $query);
 
